94deb2f4284698e007b16f784662b369b5b5a72a
Mojangcam <36653063+Mojangcam@users.noreply.github.com>
2024-05-19 13:51:43 +0900

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,4 +1,4 @@
-#define BUZZERP_PIN_NUM             6
+#define BUZZER_PIN_NUM              6
 #define ULTRASONIC_TRIG_PIN_NUM     9
 #define ULTRASONIC_ECHO_PIN_NUM     10
 
@@ -6,7 +6,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(ULTRASONIC_TRIG_PIN_NUM, OUTPUT);
   pinMode(ULTRASONIC_ECHO_PIN_NUM, INPUT);
-  pinMode(BUZZERP_PIN_NUM, OUTPUT);
+  pinMode(BUZZER_PIN_NUM, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -22,12 +22,12 @@ void loop() {
   delay(10);
   if (g_distance < 15){
       if (g_distance < 3){
-        tone(BUZZERP_PIN_NUM, 1000);
+        tone(BUZZER_PIN_NUM, 1000);
       }
       else{
-        tone(BUZZERP_PIN_NUM, 1000);
+        tone(BUZZER_PIN_NUM, 1000);
         delay(1000 - map((int)g_distance, 2, 15, 1000, 0));
-        noTone(BUZZERP_PIN_NUM);
+        noTone(BUZZER_PIN_NUM);
         delay(1000 - map((int)g_distance, 2, 15, 1000, 0));
       }
   }
